5f306909ff10348b4ed5f9c84958ceb1d2fd7249
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-12 14:39:44 +0530

    added Priority Queue info

---
+++ 03_Data-Structures/05-Queues/07_Priority_Queue.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/kth-largest-element-in-an-array/description/ (kth smallest element in array using priority queue)
+
+
+// It is a type of queue where elements are put inside the queue according to its priority :)
+
+// Elements with minimum value/maximum value can be chosen as of highest priority :)
+
+// Priority queue can be a minheap(asc order in queue) || maxheap(desc order in queue)
+
+// Insertion of a element in a n-size pq can take O(logn) worst case TC
+
+// Deletion of a element from a  n-size pq can take O(logn) worst case TC
+
+// SC : O(n) , where n is no. of elements inside the pq
+
+// Its implementation is done with the help of Tree DataStructure so please take good knowledge of trees to understand its implementation :)
+

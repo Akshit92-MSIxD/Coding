62289f82164f5bec7ee85a6a57c9a62947a8d1b9
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-14 12:49:18 +0530

    added "reverse linkedlist(data only)" problem

---
+++ 03_Data-Structures/06-Linkedlists/03_Reverse-List-Data-Only.cpp
@@ -0,0 +1,6 @@
+// Use the two pointer approach(left and right pointer) to reverse the data only :)
+// Make a function getNode() which will return the node at particular index :)
+// while(left<right){.......}
+
+
+// Note : We can also use swapping approach in which we have to swap each node data with its successive node data from left to right :)

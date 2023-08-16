10dd3f157ccec34656d191e98570e5d18d3a79a5
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-16 08:43:52 +0530

    added "kth node from end of LL" Problem

---
+++ 03_Data-Structures/06-Linkedlists/07_kth-node_from_end-of-LinkedList.cpp
@@ -0,0 +1,3 @@
+// https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1 (Find data of kth node from the end)
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/ (Delete the kth node from the end)

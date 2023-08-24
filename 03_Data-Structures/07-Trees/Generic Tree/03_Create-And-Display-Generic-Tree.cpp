71bacfaa6edc84b8f46ef2188f55a038acc11f5f
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-24 13:04:02 +0530

    added " Depth First " concept

--- 03_Data-Structures/07-Trees/Generic Tree/03_Create-And-Display-Generic-Tree.cpp
+++ 03_Data-Structures/07-Trees/Generic Tree/03_Create-And-Display-Generic-Tree.cpp
@@ -1,8 +1,12 @@
-// Create and Display Generic Tree using Node(implemented using vector approach!!!)
+// *** Imp : Please read about Depth First Creation of GTree [maine isi program mai sabse niche likha hua detail mai iske baare mai pad lena use bhi !!!!]
+
+// Depth First is related with stack !!!! :)
+// Breadth First is related with Queue !!!! :)
 
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <queue>
 using namespace std;
 
 
@@ -24,8 +28,18 @@ class Node
         this->data = data;
     }
 
+    ~Node()
+    {
+        for(Node* child : children)
+        {
+            delete child;
+        }
+    }
+
 };
 
+
+// Creating GTree using Depth First Creation (iterative)  [Note : Recursive one left]
 
 Node* createGTree(vector<int> &v)
 {
@@ -54,7 +68,8 @@ Node* createGTree(vector<int> &v)
 }
 
 
-// Display nodes using Depth First Traversal aka Euler Path Traversal :)
+
+// Display nodes using Depth First Traversal aka Euler Path Traversal recursively :) [Note : iterative one left]
 
 void display(Node* root)
 {
@@ -86,5 +101,32 @@ int main()
 
     display(root);
 
+    delete root;
+
 
 }
+
+
+
+/* Very Very Important information about Depth First Creation of Generic Tree */
+
+
+// Depth-first creation of a tree is a method for constructing a tree data structure in a way that explores as deeply as possible along a branch of the tree before backtracking. This means that it starts at the root node, creates its children, then goes deeper into the children's children, and so on, until it reaches a leaf node or a node with no more unprocessed children. When it reaches a leaf or a node with no unprocessed children, it backtracks to the nearest unprocessed ancestor node and continues the process there.
+
+// Here's a high-level description of how depth-first creation of a tree typically works:
+
+// Start with the root node.
+// Create the root node and initialize it.
+// If there are child nodes to be added, create the first child node and attach it to the root node.
+// Recursively apply the same process to the newly created child node (i.e., create its children if any).
+// Repeat steps 3 and 4 until all nodes in the branch have been created.
+// Backtrack to the nearest unprocessed ancestor node (parent node) and continue the process for its remaining children.
+// Repeat steps 3 to 6 until the entire tree has been created.
+
+// Depth-first creation is often used when you have a hierarchical or recursive structure like a tree, where you want to explore one branch of the tree as deeply as possible before moving on to the next branch. It is a common approach in various tree-based algorithms and data structures.
+
+//Depth-first creation is commonly used when constructing data structures like trees and graphs from a list of elements or when building hierarchical structures, as it often naturally follows the recursive or iterative exploration of the data.
+
+
+
+

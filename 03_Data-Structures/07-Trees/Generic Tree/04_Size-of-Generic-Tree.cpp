6f48a935f08690f2139de97ef77394fc76ac1a22
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-25 08:30:29 +0530

    added "Size of Generic Tree" problem

---
+++ 03_Data-Structures/07-Trees/Generic Tree/04_Size-of-Generic-Tree.cpp
@@ -0,0 +1,3 @@
+// Size refers to total number of nodes present in the given GTree
+// It can be find iteratively using stack
+// It can be find recursively using the expectation and faith technique :)

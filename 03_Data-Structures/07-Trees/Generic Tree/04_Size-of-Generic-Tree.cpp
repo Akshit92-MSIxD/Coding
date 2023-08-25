a708442946428cde7dd3f6465bf00962127772a7
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-25 08:32:29 +0530

    added problem link

--- 03_Data-Structures/07-Trees/Generic Tree/04_Size-of-Generic-Tree.cpp
+++ 03_Data-Structures/07-Trees/Generic Tree/04_Size-of-Generic-Tree.cpp
@@ -1,3 +1,5 @@
 // Size refers to total number of nodes present in the given GTree
 // It can be find iteratively using stack
 // It can be find recursively using the expectation and faith technique :)
+
+//https://leetcode.com/problems/count-complete-tree-nodes/

4c473f941872c5f7502aac85d443f4dd7a3907e8
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-28 11:13:43 +0530

    added "Remove Leaf nodes in a Tree" problem

---
+++ 03_Data-Structures/07-Trees/Generic Tree/10_Remove-all-leaf-Nodes-from-GTree.cpp
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/delete-leaves-with-a-given-value/description/ (Delete Leaves With a Given Value Leetcode!!!)
+
+// Note : The above problem is slightly modified and is based on BT.
+// Note : Remember DFS(postorder) will work for removing leaf nodes in BT and Gtree :)
+// Note : BFS approach left for this problem !!! (but DFS(postorder) is more optimal than BFS for this problem !!!)

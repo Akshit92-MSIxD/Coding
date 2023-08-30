481e0d87e8ecebe7f1fde752690298e899d8bbe8
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-08-30 09:28:36 +0530

    added two approaches for Flatten/Linearize a GTree

--- 03_Data-Structures/07-Trees/Generic Tree/11_Linearize-aka-Flatten-a-Tree-to_LinkedList.cpp
+++ 03_Data-Structures/07-Trees/Generic Tree/11_Linearize-aka-Flatten-a-Tree-to_LinkedList.cpp
@@ -1,5 +1,8 @@
 // https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/ (Flatten Binary Tree to Linked List Leetcode !!!)
 
+
+// Note : I have written two Linearize_GTree() functions for this problem !!!(Dono dekh lena niche!!!)
+
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -47,15 +50,17 @@ TreeNode* create_GTree(vector<int> &v)
 
 
 
-// Recursive DFS(Postorder)
+// Recursive DFS(Postorder) TC : O(n^2)
+// // Note : We are traversing from rightmost child to the leftmost child here!!!! :)
 // Convert the Gtree into a LinkedList of Preorder Sequence :)
-void flatten_GTree(TreeNode* root)
+// SC : O(n) [recursive stack space if H = n , where n is the total no. of nodes in a tree]
+void Linearize_GTree(TreeNode* root)
 {    
     // faith that childrens of root node will get flatten
 
     for(TreeNode* child : root->children)
     {
-          flatten_GTree(child); 
+          Linearize_GTree(child); 
     }
 
     // break the links between root-node(parent) and child-nodes one by one and join the tail of second-last-children flatten list to the head of last-children flatten list one by one :)
@@ -78,12 +83,41 @@ void flatten_GTree(TreeNode* root)
     return;
 }
 
+
+// // Recursive DFS(Postorder) TC : O(n)
+// // Note : We are traversing from rightmost child to the leftmost child here!!!! :)
+// // Convert the Gtree into a LinkedList of Preorder Sequence and return the tail of LinkedList :)
+//  SC : O(n) [recursive stack space if H = n , where n is the total no. of nodes in a tree]
+// TreeNode* Linearize_GTree(TreeNode* root)
+// { 
+//   if(root->children.size() == 0)
+//   return root;
+        
+      // Note : tail of entire linearized tree = tail of Linearized last child subtree
+//    TreeNode* tail_lc = Linearize_GTree(root->children.back()); // Linaerize the last child subtree + return the tail of that linearized last child subtree :) 
+
+//    while(root->children.size()>1)
+//    {
+//       TreeNode* lc = root->children.back();
+//        root->children.pop_back();
+//       TreeNode* sc = root->children.back();
+//       TreeNode* tail_sc = Linearize_GTree(sc);
+
+//       tail_sc->children.push_back(lc);
+//    }
+
+//     return tail_lc;
+// }
+
+
+
+
 int main()
 {
     vector<int> tree = {10,20,50,-1,60,-1,70,-1,-1,30,80,120,-1,-1,-1,40,90,-1,100,-1,110,-1,-1};
 
     TreeNode* root = create_GTree(tree);
-    flatten_GTree(root);
+    Linearize_GTree(root);
 
 
     // print the flatten linkedlist 
@@ -91,8 +125,10 @@ int main()
     while(root->children.size()==1)
     {
        cout<<root->data<<"-->";
-        root = root->children[0];
+       
+       root = root->children[0];
     }
+    cout<<root->data;
 
 
 

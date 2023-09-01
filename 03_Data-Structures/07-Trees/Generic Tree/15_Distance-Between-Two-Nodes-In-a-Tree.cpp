84d45602eceefbc84218186368a5a82e6aceb4c0
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-09-01 14:11:42 +0530

    added "Distance-btw-Two-Nodes-In-a-Tree" problem

---
+++ 03_Data-Structures/07-Trees/Generic Tree/15_Distance-Between-Two-Nodes-In-a-Tree.cpp
@@ -0,0 +1,8 @@
+// https://practice.geeksforgeeks.org/problems/min-distance-between-two-given-nodes-of-a-binary-tree/1 (Min distance between two given nodes of a Binary Tree)
+
+// Note : Uppar wala Problem link jo hai wo Binary Tree Ke liye hai or tu use dekhiyo pehle phir  GTree wala solution bana liyo (easy hai :))
+
+// Prerequiste : 14_Lowest-Common-Ancestor-of-GTree :)
+
+// Hint : Find LCA of two nodes a and b + Find the distance btw LCA-node and node-a + Find the distance btw LCA-node and node-b :)
+

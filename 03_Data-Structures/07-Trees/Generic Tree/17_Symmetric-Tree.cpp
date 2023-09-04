94610e5dce20caead6cb622c396e58f74e3eaa42
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-09-04 20:07:10 +0530

    added "Symmetric Tree" problem

---
+++ 03_Data-Structures/07-Trees/Generic Tree/17_Symmetric-Tree.cpp
@@ -0,0 +1,8 @@
+// A tree is symmetric  if its  right subtree is a mirror image of its left subtree :)
+
+/* Agar kisi object ko two pieces mai break kr agar dono pieces ek jaise lage par ek dusre ke mirror image jese
+  to wo  object symmetric hai  (eg : Face :) ) */
+
+// https://leetcode.com/problems/symmetric-tree/description/ (Symmtric Tree :)
+
+// Note : Maine GTree ki approaches add krdi hai Btree wali approaches mai uppar wale problem link mai jaake dekh and realte kr dono ek sath :)

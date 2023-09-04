8ad52efa4a704e1e49e7edc5a4b101f08ce33248
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-09-04 18:47:57 +0530

    added comments for checking if two trees are similar

---
+++ 03_Data-Structures/07-Trees/Generic Tree/16_Are-Trees-Similar-in-Shape.cpp
@@ -0,0 +1,8 @@
+// Note : Two trees are similar if they have identical structure and identical values(not mandatory :))
+
+// https://leetcode.com/problems/same-tree/description/ (Same Tree Leetcode)
+
+// Read the above problem first then Genric Tree ke liye bhi ban hi jaega easy hai likhne ki jarurat ni :)
+
+/* Hint : For BTree --> sath sath DFS krwaenge dono tree pe aur dono ke nodes ki values check krte rahenge :)
+   Hint : For GTree --> sath sath DFS krwaenge dono tree pe aur dono ke nodes ki values aur children ka size bhi compare krte rahenge :)  */

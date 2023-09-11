5b4e67f3f68bbeb044b5ba74dfdcb3528d18b637
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-09-11 18:01:54 +0530

    added DFS and BFS for binary tree

--- 03_Data-Structures/07-Trees/02_Binary Tree/02_DFS-Traversal.cpp
+++ 03_Data-Structures/07-Trees/02_Binary Tree/02_DFS-Traversal.cpp
@@ -1,5 +1,5 @@
-// https://leetcode.com/problems/binary-tree-preorder-traversal/[DFS prorder Traversal]
+// https://leetcode.com/problems/binary-tree-preorder-traversal/ [DFS prorder Traversal]  
 
-// https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/[DFS postorder Traversal]
+// https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/ [DFS postorder Traversal] 
 
-// https://leetcode.com/problems/binary-tree-inorder-traversal/[DFS inorder Traversal]
+// https://leetcode.com/problems/binary-tree-inorder-traversal/ [DFS inorder Traversal] 
---
+++ 03_Data-Structures/07-Trees/02_Binary Tree/03_BFS-aka-Level-Order-Traversal.cpp
@@ -0,0 +1,2 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/ (Binary Tree Level Order Traversal Leetcode!!!)
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/description/ (Binary Tree Zig Zag Level Order Traversal Leetcode!!!)

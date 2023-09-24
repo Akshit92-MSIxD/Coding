396e05d6149e9576c8620e7f1a4b4a5e27affb24
Akshit Rawat <akshitrawat.amank@gmail.com>
2023-09-24 11:56:48 +0530

    name change

--- 03_Data-Structures/07-Trees/02_Binary Tree/02_DFS-Traversal.cpp
+++ 03_Data-Structures/07-Trees/02_Binary Tree/02_DFS-Traversal.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/binary-tree-preorder-traversal/ [DFS prorder Traversal]  
+// https://leetcode.com/problems/binary-tree-preorder-traversal/ [DFS preorder Traversal]  
 
 // https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/ [DFS postorder Traversal] 
 

68833f2280f6c6f89b58bfcf420bb69bf23c71ea
Akshit Rawat <akshitrawat.amank@gmail.com>
2024-02-27 11:37:24 +0530

    minor changes

--- 06_Dynamic-Programming/03_Climbing-Stairs_with_Jumps.cpp
+++ 06_Dynamic-Programming/03_Climbing-Stairs_with_Jumps.cpp
@@ -8,42 +8,42 @@
 // TC : O(n^n)  [assume from from every step on staircase we can make upto n jumps]
 // SC : O(n) [recursive stack space]
 
-// #include <iostream>
-// #include <vector>
-// #include <unordered_map>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
 
-// int getTotalNumberOfWays(vector<int> &jumps , int start , int end)
-// {
-//     if(start == end)
-//     return 1;
+int getTotalNumberOfWays(vector<int> &jumps , int start , int end)
+{
+    if(start == end)
+    return 1;
 
-//     if(jumps[start] == 0 || start > end)
-//     return 0;
+    if(jumps[start] == 0 || start > end)
+    return 0;
         
-//      int total_ways = 0;
+     int total_ways = 0;
 
-//     for(int jmp = 1 ; jmp <= jumps[start] ; jmp++)
-//      total_ways = total_ways + getTotalNumberOfWays(jumps,start+jmp,end);
+    for(int jmp = 1 ; jmp <= jumps[start] ; jmp++)
+     total_ways = total_ways + getTotalNumberOfWays(jumps,start+jmp,end);
 
-//      return total_ways;
-// }
+     return total_ways;
+}
 
 
-// int main()
-// {
+int main()
+{
 
-//  int n; // number of stairs = n , and nth step on staircase  is the destination place !!!
-//  n = 6; // so there are total 6 stair steps on staircase and we have to find no.of ways of climbing 6 stairs from ground !!!
+ int n; // number of stairs = n , and nth step on staircase  is the destination place !!!
+ n = 6; // so there are total 6 stair steps on staircase and we have to find no.of ways of climbing 6 stairs from ground !!!
 
-//  vector<int> jumps = {3,3,0,2,2,3}; // from 0th step i.e ground you can make upto 2 jumps , from 1th step --> upto 4 jumps , from 2th step --> upto 1 jump ,...........from 5th step upto 3 jumps and the 6th step is the destination place/step on staircase !!!
+ vector<int> jumps = {3,3,0,2,2,3}; // from 0th step i.e ground you can make upto 2 jumps , from 1th step --> upto 4 jumps , from 2th step --> upto 1 jump ,...........from 5th step upto 3 jumps and the 6th step is the destination place/step on staircase !!!
 
-//  int total_ways = getTotalNumberOfWays(jumps,0,n);
+ int total_ways = getTotalNumberOfWays(jumps,0,n);
 
-//  cout<<"\nTotal number of ways of climbing "<<n<<" stairs from ground : "<<total_ways<<endl<<endl;
+ cout<<"\nTotal number of ways of climbing "<<n<<" stairs from ground : "<<total_ways<<endl<<endl;
 
-// }
+}
 
 
 
@@ -113,42 +113,42 @@
 // TC : O(n^2)  [assume from from every step on staircase we can make upto n jumps]
 // SC : O(n) [vector space]
 
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// using namespace std;
 
-int main()
-{
+// int main()
+// {
 
- int n; // number of stairs = n , and nth step on staircase  is the destination place !!!
- n = 6; // so there are total 6 stair steps on staircase and we have to find no.of ways of climbing 6 stairs from ground !!!
+//  int n; // number of stairs = n , and nth step on staircase  is the destination place !!!
+//  n = 6; // so there are total 6 stair steps on staircase and we have to find no.of ways of climbing 6 stairs from ground !!!
 
- vector<int> jumps = {3,3,0,2,2,3}; // from 0th step i.e ground you can make upto 2 jumps , from 1th step --> upto 4 jumps , from 2th step --> upto 1 jump ,...........from 5th step upto 3 jumps and the 6th step is the destination place/step on staircase !!!
+//  vector<int> jumps = {3,3,0,2,2,3}; // from 0th step i.e ground you can make upto 2 jumps , from 1th step --> upto 4 jumps , from 2th step --> upto 1 jump ,...........from 5th step upto 3 jumps and the 6th step is the destination place/step on staircase !!!
 
 
- vector<int> dp(n+1,0);   // assume each index i represent no. of ways to reach nth stair from ith stair 
+//  vector<int> dp(n+1,0);   // assume each index i represent no. of ways to reach nth stair from ith stair 
 
- dp[n] = 1;
+//  dp[n] = 1;
 
- for(int i = n-1 ; i>=0 ; i--)   // In this case smaller subproblem will exist RHS and main problem will exist on LHS of dp vector so we traverse the dp vector in reverse direction !!!
- {
+//  for(int i = n-1 ; i>=0 ; i--)   // In this case smaller subproblem will exist RHS and main problem will exist on LHS of dp vector so we traverse the dp vector in reverse direction !!!
+//  {
        
-       for(int jmp = 1; jmp<=jumps[i];jmp++)
-       {
-           if(i+jmp > n)
-           break;
+//        for(int jmp = 1; jmp<=jumps[i];jmp++)
+//        {
+//            if(i+jmp > n)
+//            break;
 
-           dp[i] = dp[i] + dp[i+jmp];
-       }
+//            dp[i] = dp[i] + dp[i+jmp];
+//        }
 
- }
+//  }
 
 
 
- cout<<"\nTotal number of ways of climbing "<<n<<" stairs from ground : "<<dp[0]<<endl<<endl;
+//  cout<<"\nTotal number of ways of climbing "<<n<<" stairs from ground : "<<dp[0]<<endl<<endl;
 
-}
+// }
 
 
 

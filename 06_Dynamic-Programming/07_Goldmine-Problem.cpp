8a3473eef9c627759d13c599a209c84fc8389068
Akshit Rawat <akshitrawat.amank@gmail.com>
2024-02-28 19:35:08 +0530

    added "Goldmine + Path with Max Gold(backtracking)" problem

---
+++ 06_Dynamic-Programming/07_Goldmine-Problem.cpp
@@ -0,0 +1,3 @@
+// https://www.geeksforgeeks.org/problems/gold-mine-problem2608/1
+
+// https://leetcode.com/problems/path-with-maximum-gold/ [This is a backtracking problem not a dp problem but its related problem !!!]

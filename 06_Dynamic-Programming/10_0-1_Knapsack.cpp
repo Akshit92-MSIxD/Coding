a50bf9ea4f3ec9a71f6227ff7ddaf2525865c921
Akshit Rawat <akshitrawat.amank@gmail.com>
2024-03-11 23:00:12 +0530

    added "Zeroes and Ones" Problem

--- 06_Dynamic-Programming/10_0-1_Knapsack.cpp
+++ 06_Dynamic-Programming/10_0-1_Knapsack.cpp
@@ -1,1 +1,3 @@
 // https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
+
+// https://leetcode.com/problems/ones-and-zeroes/description/ (Zeroes and Ones Leetcode)(Related Problem!!!)

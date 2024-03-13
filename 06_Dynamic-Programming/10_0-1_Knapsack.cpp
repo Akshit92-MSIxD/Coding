945ab1d866b414fc84b0374f997900955bc651d9
Akshit Rawat <akshitrawat.amank@gmail.com>
2024-03-13 11:30:29 +0530

    minor changes

--- 06_Dynamic-Programming/10_0-1_Knapsack.cpp
+++ 06_Dynamic-Programming/10_0-1_Knapsack.cpp
@@ -1,3 +1,5 @@
 // https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
 
+// *** Note : Watch Strivers Video on 1D array Optimisation of 0-1 kanpsack !!!!
+
 // https://leetcode.com/problems/ones-and-zeroes/description/ (Zeroes and Ones Leetcode)(Related Problem!!!)

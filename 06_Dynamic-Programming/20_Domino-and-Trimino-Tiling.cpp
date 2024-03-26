f6b3d3408316f7e254463b7f2b487b29b9b5e1d1
Akshit Rawat <akshitrawat.amank@gmail.com>
2024-03-26 14:11:19 +0530

    minor change

--- 06_Dynamic-Programming/20_Domino-and-Trimino-Tiling.cpp
+++ 06_Dynamic-Programming/20_Domino-and-Trimino-Tiling.cpp
@@ -1,3 +1,3 @@
-// https://leetcode.com/problems/domino-and-tromino-tiling/description/ (Domino and Trimino Tiling !!!)
+// https://leetcode.com/problems/domino-and-tromino-tiling/description/ (Domino and Trimino Tiling )
 
 // Watch : https://www.youtube.com/watch?v=Iwmn-gFL3c0&t=407s (For optimal approach !!!)

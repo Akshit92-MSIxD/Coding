6a973e9b72992091258266ede3ef1604e5104dcf
Akshit Rawat <akshitrawat.amank@gmail.com>
2025-11-27 23:19:51 +0530

    added graph connected component problems

--- 03_Data-Structures/09_Graphs/05_Get-Connected-Components.java
+++ 03_Data-Structures/09_Graphs/05_Get-Connected-Components.java
@@ -1,2 +1,3 @@
 // http://geeksforgeeks.org/problems/connected-components-in-an-undirected-graph/1 
 // https://leetcode.com/problems/count-the-number-of-complete-components/ (Slight variation of above question !!!)
+// https://leetcode.com/problems/number-of-islands/( This problem is related to above questions !!!)

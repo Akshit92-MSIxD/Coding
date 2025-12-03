ce2c70fa855bbba2a964db654e135372895ac4cc
Akshit Rawat <akshitrawat.amank@gmail.com>
2025-12-03 17:31:00 +0530

    added knights tour problem

---
+++ 03_Data-Structures/09_Graphs/08_Knights-Tour.java
@@ -0,0 +1,4 @@
+// These are two problems completely related to Knights Tour !!!
+
+// https://www.geeksforgeeks.org/problems/hamiltonian-path2522/1 (First Solve this problem then the below one !!!)
+// https://leetcode.com/problems/check-knight-tour-configuration/submissions/1845872229/

27e125238f6b0e8aca2f522e4ad36e42a1b7f3f9
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-03-08 14:07:00 +0530

    added shortest path in weighted graph problem

--- 03_Data-Structures/09_Graphs/13_Dijkstra-Algorithm.java
+++ 03_Data-Structures/09_Graphs/13_Dijkstra-Algorithm.java
@@ -1,2 +1,4 @@
 // ***IMP : Please read about heaps and the TC and SC of their operations to better analyze the TC and SC of this algorithm !!!
 // https://www.geeksforgeeks.org/problems/implementing-dijkstra-set-1-adjacency-matrix/1
+
+// https://www.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1 (Shortest Path in an Undirected Graph)

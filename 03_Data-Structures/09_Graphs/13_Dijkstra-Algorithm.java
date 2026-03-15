563b136db940fcee65028f3c7a0c61643f9994cc
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-03-15 13:09:29 +0530

    Added no. of ways to arrive at destination problem

--- 03_Data-Structures/09_Graphs/13_Dijkstra-Algorithm.java
+++ 03_Data-Structures/09_Graphs/13_Dijkstra-Algorithm.java
@@ -8,3 +8,5 @@
 // https://leetcode.com/problems/cheapest-flights-within-k-stops/ (TC aur SC samajane mai dikkat aari !!!)
 
 // https://www.geeksforgeeks.org/problems/minimum-multiplications-to-reach-end/1
+
+// https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/ 

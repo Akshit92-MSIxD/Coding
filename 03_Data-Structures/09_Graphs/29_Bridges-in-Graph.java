9f49bc4fc78dd7a8f9d40b14f70623b571623e6b
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-03-31 18:12:45 +0530

    added Bridges in a graph problem

---
+++ 03_Data-Structures/09_Graphs/29_Bridges-in-Graph.java
@@ -0,0 +1,3 @@
+// https://www.geeksforgeeks.org/problems/bridge-edge-in-graph/1 (Understand this first to properly understand how and why tarjens algo works !!!)
+
+// https://leetcode.com/problems/critical-connections-in-a-network/ (Strivers Approach for Tarjen's Algorithm)

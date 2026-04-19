fa259f649730e353fe0c9928adfda06b4ee5d0e0
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-04-19 12:47:16 +0530

    added two problem i.e cycle detection in directed graphs and Eventual safe states

--- 03_Data-Structures/09_Graphs/10_Is-Graph-Cyclic.java
+++ 03_Data-Structures/09_Graphs/10_Is-Graph-Cyclic.java
@@ -1,3 +1,7 @@
 // https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
 // https://leetcode.com/problems/detect-cycles-in-2d-grid/submissions/1974195049/
+
+// https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
+
+// https://leetcode.com/problems/find-eventual-safe-states/submissions/ (Please write proper comments and watch video solution for this problem !!!)

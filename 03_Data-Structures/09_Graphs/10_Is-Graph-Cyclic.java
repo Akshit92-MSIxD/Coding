1ae093891894d8867f919aa827378ec2002b898b
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-04-21 12:07:45 +0530

    minor changes

--- 03_Data-Structures/09_Graphs/10_Is-Graph-Cyclic.java
+++ 03_Data-Structures/09_Graphs/10_Is-Graph-Cyclic.java
@@ -4,4 +4,4 @@
 
 // https://www.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1
 
-// https://leetcode.com/problems/find-eventual-safe-states/submissions/ (Please write proper comments and watch video solution for this problem !!!)
+// https://leetcode.com/problems/find-eventual-safe-states/submissions/ 

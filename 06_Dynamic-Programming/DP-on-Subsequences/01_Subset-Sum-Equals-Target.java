fa8540c988d5a4aee3065d2f86ae492f52a4d274
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-05-14 12:13:26 +0530

    added  Partition A Set Into Two Subsets With Minimum Absolute Sum Difference Problem

--- 06_Dynamic-Programming/DP-on-Subsequences/01_Subset-Sum-Equals-Target.java
+++ 06_Dynamic-Programming/DP-on-Subsequences/01_Subset-Sum-Equals-Target.java
@@ -1,2 +1,3 @@
 // https://www.geeksforgeeks.org/problems/subset-sum-problem-1611555638/1?itm_source=geeksforgeeks&itm_medium=article&itm_campaign=bottom_sticky_on_article
 // https://leetcode.com/problems/partition-equal-subset-sum/ (Partion DP problem !!!)
+// https://www.geeksforgeeks.org/problems/minimum-sum-partition3317/1 ( Partition a Set Into Two Subsets With Minimum Absolute Sum Difference)

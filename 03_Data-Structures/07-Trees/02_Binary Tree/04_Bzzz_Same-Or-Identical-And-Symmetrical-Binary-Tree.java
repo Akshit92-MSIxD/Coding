dd2aebe897e91a7f434d6a01267957d892d9345b
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-06-04 01:05:56 +0530

    added Identical and Symmetric Binary Tree problem

---
+++ 03_Data-Structures/07-Trees/02_Binary Tree/04_Bzzz_Same-Or-Identical-And-Symmetrical-Binary-Tree.java
@@ -0,0 +1,2 @@
+// https://leetcode.com/problems/same-tree/description/ (Same/Identical Binary Tree) [*** First Solve this problem then the below one !!!]
+// https://leetcode.com/problems/symmetric-tree/ (Symmetric Tree)

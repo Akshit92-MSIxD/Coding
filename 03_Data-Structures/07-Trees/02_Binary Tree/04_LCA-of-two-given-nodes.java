3a842ff0a49381fcb4d8578a0b1031570aee66e1
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-06-04 18:33:56 +0530

    added LCA of a given two nodes problem

---
+++ 03_Data-Structures/07-Trees/02_Binary Tree/04_LCA-of-two-given-nodes.java
@@ -0,0 +1,3 @@
+// ***NOTE : Please learn Binary Lifting method (Advanced Tree Method). It is useful when we have to find LCA for 100000 queries !!! 
+
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/submissions/2022301741/

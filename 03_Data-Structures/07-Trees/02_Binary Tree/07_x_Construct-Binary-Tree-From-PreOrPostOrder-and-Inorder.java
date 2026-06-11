55ff1edddefeb136e7618b6639c2c3e74ef74679
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-06-11 13:57:16 +0530

    added Construct Binary Tree from PostOrder and Inorder

---
+++ 03_Data-Structures/07-Trees/02_Binary Tree/07_x_Construct-Binary-Tree-From-PreOrPostOrder-and-Inorder.java
@@ -0,0 +1,6 @@
+
+// https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+
+
+// (Please solve all three approaches of above problem then only solve this below one !!!)
+// https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/description/ 

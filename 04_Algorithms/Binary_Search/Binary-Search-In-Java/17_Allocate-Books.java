5fba92459c38c370e7e808f958a749b70c17aec8
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-07-10 18:32:43 +0530

    added comment

--- 04_Algorithms/Binary_Search/Binary-Search-In-Java/17_Allocate-Books.java
+++ 04_Algorithms/Binary_Search/Binary-Search-In-Java/17_Allocate-Books.java
@@ -1,3 +1,6 @@
+
+// Note : Please solve these problems using DP approach also !!!
+
 // https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
 // https://leetcode.com/problems/split-array-largest-sum/submissions/
 // https://www.geeksforgeeks.org/problems/the-painters-partition-problem1535/1

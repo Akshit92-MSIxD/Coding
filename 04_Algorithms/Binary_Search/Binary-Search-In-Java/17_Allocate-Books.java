64eb1a132d559325a05b67a7fb219fe99953b097
Akshit Rawat <akshitrawat.amank@gmail.com>
2026-07-10 18:27:43 +0530

    added painters partition problem

--- 04_Algorithms/Binary_Search/Binary-Search-In-Java/17_Allocate-Books.java
+++ 04_Algorithms/Binary_Search/Binary-Search-In-Java/17_Allocate-Books.java
@@ -1,2 +1,3 @@
 // https://www.geeksforgeeks.org/problems/allocate-minimum-number-of-pages0937/1
 // https://leetcode.com/problems/split-array-largest-sum/submissions/
+// https://www.geeksforgeeks.org/problems/the-painters-partition-problem1535/1
